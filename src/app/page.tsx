89cce9a9844b7992cb8772ace4d262ebe54f9d84
unknown <wpsdoc9@gmail.com>
2026-05-23 15:59:28 +0100

    delete: removed Showcase section completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { Features } from "@/components/sections/Features";
-import { Showcase } from "@/components/sections/Showcase";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
-      <Showcase />
       <Footer />
     </main>
   );
